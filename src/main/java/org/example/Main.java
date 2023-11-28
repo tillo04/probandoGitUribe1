5ba2e984f5d3d0244b31279990d74012a61b65ad
stevenarango <stcajugl@gmail.com>
2023-11-28 11:30:07 -0500

    se inicia proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    //natilleraLaAbuelitaToreto
+    //¿queNecesitoParaProcesarLaNatilleraDeUnCliente?
+    //ENTRADAS
+    int identificadorCliente=233444;
+    long documentoIdentidad=1001234067000L;
+    byte edadCliente=34;
+    short cuotaMensualCliente=25000;
+    float tasaDeInteresFijo=0.1F;
+    char calificacionCliente='A';
+
+
+}
